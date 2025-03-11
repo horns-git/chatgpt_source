ec2ebf9d947e864a573de141e54db8a6b35372cf
Horns Hooves <horns.hooves.unlimited@gmail.com>
2025-03-11 18:52:01 +0300

    Update hh_core.py

--- hh_core.py
+++ hh_core.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import math
-import gc
 
 ### MultiIndex level slice constant:
 every = slice(None)
@@ -84,71 +83,6 @@ def gen_source_dataset(str_date_start='2000-01-01', str_date_end=(pd.Timestamp.n
     except Exception as err:
         print('Error generating DataFrame: %s', str(err))
         return None
-
-### DEFINING COUNTRY CODES EXTRACTOR
-def get_country_codes(use_local_copy = False):  
-    ### In case if URL is unavailable:
-    if (use_local_copy):
-        url_country_code = 'Data_Files/Source_Files/countrycode.html'
-    ### Online extraction:
-    else:
-        url_country_code = 'https://countrycode.org/'
-    df_full_codes = pd.read_html(url_country_code, index_col = 'COUNTRY')[0]
-    df_full_codes[['ISO SHORT', 'ISO LONG']] = df_full_codes['ISO CODES'].str.split(' / ', expand = True)
-    df_result = df_full_codes[['ISO SHORT', 'ISO LONG']].sort_index()    
-    df_result.index = df_result.index.str.upper()
-    ### Results output:
-    return df_result
-
-### DEFINING EXTRACTION UNIVERSE DATA FROM MS EXCEL SOURCE
-def ison_membership_converting(str_path_universe, date_end, bool_daily = False, int_backfill_months = 0):
-    ### Defining business-month-end reindexation on country level:
-    def country_modify(ser_raw_country, date_end):
-        ser_res_country = ser_raw_country.droplevel(0).resample('MS').last().resample('BM').last()
-        range_country = pd.date_range(ser_res_country.index[0], date_end, freq = 'BM')
-        return ser_res_country.reindex(range_country).ffill()
-    ### Markets encoding table:
-    dict_markets = {50 : 'DM', 57 : 'EM', 504 : 'FM', 0: np.NaN}     
-    ### Loading source file:
-    df_raw_universe = pd.read_excel(engine = 'openpyxl', io = str_path_universe, sheet_name = 'Switchers', header = 0, index_col = [0, 1],
-                                    parse_dates = [1, 2], date_format='%m/%d/%Y', 
-                                    na_values = list_na_excel_values, keep_default_na = False)
-    ### Converting source file:
-    df_raw_universe.index.names = ['Country', 'Date']
-    ser_raw_universe = df_raw_universe['Region']
-    ser_raw_universe.fillna(0, inplace = True)
-    ser_raw_universe.name = 'Market'
-    ### By country reindexation and translation:
-    ser_res_universe = ser_raw_universe.groupby('Country').apply(country_modify, date_end)
-    ser_res_universe.index.names = ['Country', 'Date']
-    ser_res_universe = ser_res_universe.replace(dict_markets).reorder_levels([1, 0]).sort_index() 
-    ### Expanding membership for primary regions members by backfilling:
-    if int_backfill_months:
-        ### List of regions:
-        list_region = list(ser_res_universe.dropna().unique())
-        ### Initialising of collection of series with backfilled data for each region:
-        list_ison_backfill = []
-        ### Regions looping:
-        for iter_region in list_region:
-            ### Defining start of region date:
-            date_first_valid = ser_res_universe.loc[ser_res_universe == iter_region].first_valid_index()[0]
-            ### Creating dates index to backfilling:
-            idx_date_backfill = pd.date_range(end = date_first_valid, periods = int_backfill_months + 1, freq = 'BM')[: -1]
-            ### Creating primary countries index to backfilling:            
-            idx_region_backfill = ser_res_universe.loc[ser_res_universe == iter_region].loc[date_first_valid, All].index.get_level_values('Country')
-            ### Creating full index:
-            idx_ison_backfill = pd.MultiIndex.from_product([idx_date_backfill, idx_region_backfill])
-            ### Series with backfilled data:
-            list_ison_backfill.append(pd.Series(iter_region, index = idx_ison_backfill))
-        ### Combination of backfilled series and original ISON data:    
-        ser_res_universe = ser_res_universe.combine_first(pd.concat(list_ison_backfill, axis = 0)).sort_index()  
-        ser_res_universe.index.names = ['Date', 'Country']
-    ### Converting to daily frequency:
-    if bool_daily:
-        ser_res_universe = ser_res_universe.reset_index('Country').groupby('Country').resample('B').ffill()['Market'].swaplevel().sort_index()    
-    ### Results output:
-    ser_res_universe.name = 'Market'
-    return ser_res_universe    
 
 ### DEFINING WEIGHTED AVERAGE FOR DATAFRAME COLUMNS
 def columns_average(df_series, list_weights = False): 
@@ -271,16 +205,3 @@ def weighted_average_grouped(ser_country, ser_weight = None):
     else:
         flo_wa = np.NaN
     return flo_wa
-
-### DEFINING MULTI-STEP STANDARTIZATION BY MARKET FOR CROSS-SECTION
-def ison_standartize(ser_to_manage, arr_truncate, ser_weight = False, reuse_outliers = False, center_result = True, full_result = False, within_market = False):
-    ### Multi-step standartizing:
-    if (within_market):
-    ### Within market standartizing:
-        ser_result = ser_to_manage.groupby(by = 'Market', group_keys = False).apply(multistep_standartize, arr_truncate, ser_weight, 
-                                                                                                  reuse_outliers, center_result, full_result)
-    else:
-    ### Full universe standartizing:
-        ser_result = multistep_standartize(ser_to_manage, arr_truncate, ser_weight, reuse_outliers, center_result, full_result)
-    ### Results output:
-    return ser_result
